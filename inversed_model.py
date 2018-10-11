e09b753cc8c11589d108c3727c4010b6cfef5553
Takafumi Magara <tmagara@nifty.com>
2018-10-11 09:13:12 +0900

    Rewrite inversed model with no monotonic constraints.

--- inversed_model.py
+++ inversed_model.py
@@ -1,49 +1,48 @@
 import chainer
 
-import models
 
-
-class InversedProbabilityDistributionNetwork(chainer.ChainList):
-    def __init__(self, pdn, output_size, monotone_size, marginal_size, pool_size):
+class InversedProbabilityDistributionNetwork(chainer.Chain):
+    def __init__(self, pdn, output_size):
         super().__init__()
         self.pdn = pdn  # Don't register as a link. We don't want to train it here.
-        for i in range(output_size):
-            net = models.MonotonicNetwork([1] + monotone_size, [i] + marginal_size, pool_size)
-            self.add_link(net)
+        units = [output_size, 64, 128, 64, output_size]
+        initializer = chainer.initializers.HeNormal()
+        with self.init_scope():
+            self.linear0 = chainer.links.Linear(units[0], units[1], initialW=initializer)
+            self.linear1 = chainer.links.Linear(units[1], units[2], initialW=initializer)
+            self.linear2 = chainer.links.Linear(units[2], units[3], initialW=initializer)
+            self.linear3 = chainer.links.Linear(units[3], units[4], initialW=initializer)
 
-    def sample(self, x):
-        xp = self.xp
-        p0 = xp.random.uniform(0.0, 1.0, x.shape).astype(x.dtype)
-        xi_list = []
-        for i, net in enumerate(self):
-            p_i = p0[:, i:i + 1]
-            if len(xi_list) == 0:
-                marginal = xp.zeros((p_i.shape[0], 0), p_i.dtype)
-            else:
-                marginal = chainer.functions.concat(tuple(xi_list), 1)
-            xi = net(p_i, marginal)
-            xi_list.append(xi.data)
-        x = xp.concatenate(tuple(xi_list), 1)
-        return x
+    def sample(self, p0):
+        h = p0
 
-    def __call__(self, x):
+        h = self.linear0(h)
+        h = chainer.functions.relu(h)
+        h = self.linear1(h)
+        h = chainer.functions.relu(h)
+        h = self.linear2(h)
+        h = chainer.functions.relu(h)
+        h = self.linear3(h)
+
+        return h
+
+    def log_mse(self, x0, x1, axis):
+        dd = chainer.functions.square(x0 - x1)
+        mse = chainer.functions.mean(dd, axis, keepdims=True)
+        log_mse = 0.5 * chainer.functions.log(mse)
+        return log_mse
+
+    def __call__(self, x0):
         xp = self.xp
-        p0 = xp.random.uniform(0.0, 1.0, x.shape).astype(x.dtype)
-        xi_list = []
-        loss = []
-        for i, net in enumerate(self):
-            p_i = p0[:, i:i + 1]
-            if len(xi_list) == 0:
-                marginal = xp.zeros((p_i.shape[0], 0), p_i.dtype)
-            else:
-                marginal = xp.concatenate(tuple(xi_list), 1)
-            xi = net(p_i, marginal)
-            yi = self.pdn[i](xi, marginal)
-            loss_i = chainer.functions.bernoulli_nll(p_i, yi)
-            loss_i = chainer.functions.sum(loss_i) / loss_i.size
-            xi_list.append(xi.data)
-            loss.append(loss_i)
-        loss = chainer.functions.stack(tuple(loss))
-        loss = chainer.functions.sum(loss)
-        chainer.report({'loss': loss}, self)
-        return loss
+        p0 = xp.random.uniform(0.0, 1.0, x0.shape).astype(x0.dtype)
+
+        x = self.sample(p0)
+        y = self.pdn.cumulative_p(x)
+        p = chainer.functions.sigmoid(y)
+
+        # p_loss = chainer.functions.bernoulli_nll(p0, y) / y.size
+        p_loss = self.log_mse(p0, p, (0, ))
+        p_loss = chainer.functions.sum(p_loss)
+
+        chainer.report({'loss': p_loss}, self)
+        return p_loss
